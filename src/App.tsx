2a2b764e7e276498a3a43a6a709fce7394a7ebd9
Nguyen Hong Duc <autoclickvn@gmail.com>
2024-11-29 08:31:07 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div style={{ color: '#FFF' }}>React - Information</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  devServer: {
+    port: 4001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'reactApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App' // Export component App
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: '18.0.0' },
+        'react-dom': { singleton: true, eager: true, requiredVersion: '18.0.0' }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
